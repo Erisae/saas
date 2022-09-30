62f2c3ff6fb7c81120430b353fd1fe45c8e15a3d
Yuhan Xia <yx2729@columbia.edu>
2022-09-30 16:32:43 +0000

    add end in movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
     return self.group("Rating")
+  end 
+  
   def self.with_ratings(ratings_list)
     if ratings_list.length == 0 #nil
       return self.all
