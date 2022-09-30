9f42ae7db107e592a19a708b4bccf3c715258c07
Yuhan Xia <yx2729@columbia.edu>
2022-09-30 21:15:03 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,6 +27,8 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully created."
     # controller sets this variable by consulting the Model
     @all_ratings = Movie.all_ratings # my
+    puts "======================"
+    print @all_ratings
     # a collection of which ratings should be checked, array
     @ratings_to_show = [] # my
     redirect_to movies_path
