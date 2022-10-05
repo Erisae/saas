9e2fd9ca97715306c3fdb22c5cccbd37e88cf275
Yuhan Xia <yx2729@columbia.edu>
2022-10-05 03:42:54 +0000

    www

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,11 +14,12 @@ class MoviesController < ApplicationController
       elsif session[:reorder] == "release_date"
         clicking_v = {:movie_title=>"0", :release_date=>"1"}
       end
+      params[:clicking] = session[:reorder]
 
       if session[:ratings] != nil
         rating_v = Hash[session[:ratings].map {|v| [v,"1"]}]
       end
-      
+      params[:ratings] = session[:ratings]
       redirect_to movies_path({:clicking=>clicking_v, :ratings=>rating_v}) and return
     end
 
